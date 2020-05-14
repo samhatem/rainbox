1b6ea0dbb071bd4a8dcda576e51f8312dd35c5f8
Zach Ferland <zachferland@gmail.com>
2020-05-14 15:49:53 -0400

    feat: 3idconnect with eth provider

--- example/index.js
+++ example/index.js
@@ -3,12 +3,10 @@ const syncComplete = (res) => {
   updateProfileData(window.box)
 }
 
-Box.get3idConnectProvider().then(provider => {
-  Box.create(provider).then(box => {
-    window.box = box
-    bauth.disabled = false
-    openThread.disabled = false
-  })
+Box.create(window.ethereum).then(box => {
+  window.box = box
+  bauth.disabled = false
+  openThread.disabled = false
 })
 
 bauth.addEventListener('click', event => {
